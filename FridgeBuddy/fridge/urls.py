eb157f950d58d31981d792741f03f52b7d9f8e6e
Krystine Xie <krystinex@gmail.com>
2020-11-17 13:27:50 -0800

    added models, views, urls content to fridge app

--- FridgeBuddy/fridge/urls.py
+++ FridgeBuddy/fridge/urls.py
@@ -2,5 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.has_fridge_dash), 
+    path('/new_user', views.new_fridge_dash),
+    path('/new', views.create_fridge_page),
+    path('/new_fridge_success', views.create_fridge_success),
+    path('/join_fridge', views.join_fridge_page),
+    path('/join_success', views.join_fridge)
+
 ]
--- FridgeBuddy/login_and_rego/models.py
+++ FridgeBuddy/login_and_rego/models.py
@@ -45,6 +45,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
     email = models.EmailField(max_length=100)
+    fridge = models.ForeignKey('fridge.Fridge', related_name="members", on_delete = models.CASCADE)
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
